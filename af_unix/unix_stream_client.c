b25ef054e5e46d17b31598614c8487852ea1a486
panyunfei <panyunfei@zdns.cn>
2024-04-07 14:40:54 +0800

    [#] test af_unix socket

---
+++ af_unix/unix_stream_client.c
@@ -0,0 +1,51 @@
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <errno.h>
+#include <stdio.h>
+#include <linux/un.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define MAXLINE 2048
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        error(1, 0, "usage: unixstreamclient <local_path>");
+    }
+
+    int sockfd;
+    struct sockaddr_un servaddr;
+
+    sockfd = socket(AF_LOCAL, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        error(1, errno, "create socket failed");
+    }
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sun_family = AF_LOCAL;
+    strcpy(servaddr.sun_path, argv[1]);
+
+    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        error(1, errno, "connect failed");
+    }
+
+    char send_line[MAXLINE];
+    bzero(send_line, MAXLINE);
+    char recv_line[MAXLINE];
+
+    while (fgets(send_line, MAXLINE, stdin) != NULL) {
+
+        int nbytes = sizeof(send_line);
+        if (write(sockfd, send_line, nbytes) != nbytes)
+            error(1, errno, "write error");
+
+        if (read(sockfd, recv_line, MAXLINE) == 0)
+            error(1, errno, "server terminated prematurely");
+
+        fputs(recv_line, stdout);
+    }
+
+    exit(0);
+}
+
---
+++ af_unix/unix_stream_server.c
@@ -0,0 +1,75 @@
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <errno.h>
+#include <stdio.h>
+#include <linux/un.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define LISTENQ 10
+#define BUFFER_SIZE 1024
+#define MAXLINE 2048
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        error(1, 0, "usage: unixstreamserver <local_path>");
+    }
+
+    int listenfd, connfd;
+    socklen_t clilen;
+    struct sockaddr_un cliaddr,servaddr;
+
+    listenfd = socket(AF_LOCAL, SOCK_STREAM, 0);
+    if (listenfd < 0) {
+        error(1, errno, "socket create failed");
+    }
+
+    const char *local_path = argv[1];
+    unlink(local_path);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sun_family = AF_LOCAL;
+    strcpy(servaddr.sun_path, local_path);
+
+    if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        error(1, errno, "bind failed");
+    }
+
+    if (listen(listenfd, LISTENQ) < 0) {
+        error(1, errno, "listen failed");
+    }
+
+    clilen = sizeof(cliaddr);
+    if ((connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen)) < 0) {
+        if (errno == EINTR)
+            error(1, errno, "accept failed");        /* back to for() */
+        else
+            error(1, errno, "accept failed");
+    }
+
+    char buf[BUFFER_SIZE];
+
+    while (1) {
+        bzero(buf, sizeof(buf));
+        if (read(connfd, buf, BUFFER_SIZE) == 0) {
+            printf("client quit");
+            break;
+        }
+        printf("Receive: %s", buf);
+
+        char send_line[MAXLINE];
+        sprintf(send_line, "Hi, %s", buf);
+
+        int nbytes = sizeof(send_line);
+
+        if (write(connfd, send_line, nbytes) != nbytes)
+            error(1, errno, "write error");
+    }
+
+    close(listenfd);
+    close(connfd);
+
+    exit(0);
+
+}
+
